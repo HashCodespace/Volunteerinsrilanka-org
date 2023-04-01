8ffb1a52e63c00614a61725efcc6924a2b5b868a
HashCodespace <emhashenudara@gmail.com>
2023-04-01 12:13:42 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,39 @@
 const { DateTime } = require("luxon")
+const Image = require("@11ty/eleventy-img");
 
+
+async function imageShortcode(src, alt, sizes , style, classlist) {
+  let metadata = await Image(src, {
+    widths: [750, 600, 1000,1200],
+    formats: ["webp"],
+    urlPath: "/images/",
+    outputDir: "./dist/images/",
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    style,
+    classlist,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You can add other attributes such as `class` and `style` here
+  return Image.generateHTML(metadata, imageAttributes);
+}
 
 
 module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy('./src/assets')
   eleventyConfig.addPassthroughCopy('./src/.htaccess')
   // eleventyConfig.addPassthroughCopy('./src/admin')
+  eleventyConfig.addAsyncShortcode("image", imageShortcode);
+
+  // Or add them individually
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
 
   eleventyConfig.addFilter("postDate", (dataObj) => {
     return DateTime.fromJSDate(dataObj).toLocaleString(DateTime.DATE_MED);
@@ -13,7 +41,7 @@ module.exports = function(eleventyConfig){
   
     // Return your Object options:
   return {
-    markdownTemplateEngine: "md",
+    markdownTemplateEngine: "njk",
     dir: {
       input: "src",
       output: "dist"
